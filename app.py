240a877056a3c8de6c221fd6f11045f3bb35f183
kirti101 <85452725+kirti101@users.noreply.github.com>
2022-11-23 18:04:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,6 +114,8 @@ def gen_frames():
 def index():
 	return render_template('index.html')
 
+# video feed
+
 @app.route('/video_feed')
 def video_feed():
 	return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
